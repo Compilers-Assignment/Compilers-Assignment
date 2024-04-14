34d4bf9ceaef355a75d9857064fdb121c75b8a49
ErkMagnifico <ssj3amitghosh@gmail.com>
2024-04-14 12:33:21 +0530

    fix: output format now followed, implemented some parts of program  block

--- sample.c
+++ sample.c
@@ -3,6 +3,14 @@ program hello_world;
 var
 
 var_list : real;
-this, you, : integer;
+this, you : integer;
+
+begin
+
+Write("hello world");
+read(this);
+ghot := bailesh;
+ghot := pailesh;
+bhot := gailesh;
 
 
